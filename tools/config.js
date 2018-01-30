e83de4ea682e308d7b7cc5f3874c6a6adfb62f2f
vinay-curatio <vinay@curatio.me>
2018-01-30 13:59:36 -0800

    Changed favivon and updated website title

--- tools/config.js
+++ tools/config.js
@@ -9,7 +9,7 @@
  */
 
 module.exports = {
-  title: 'React Static Boilerplate',        // Your website title
+  title: 'Todo App',        // Your website title
   url: 'https://rsb.kriasoft.com',          // Your website URL
   project: 'react-static-boilerplate',      // Firebase project. See README.md -> How to Deploy
   trackingID: 'UA-XXXXX-Y',                 // Google Analytics Site's ID
